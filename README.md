db31f2d0fa9ac6e34a6cd026349ab2db3aa884a6
paul van bladel <paul.vanbladel@gmail.com>
2016-10-10 17:50:26 +0200

    update readme and formatting

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Redux-demystified 
 
-## Installation
 
+## Introduction
+
+patterns like Redux are complex. Stepping back and bringing things back to simple categories can help in understanding the underlying plumbing.
+This repository demonstrates in nodeJs with typescript (by making use of ts-node) how following Redux building blocks play together:
+* a store
+* actions
+* reducers
+
+The sample how both a non-rx and a rx implementation.
+
+## Installation
 ```sh
 npm install -g ts-node
 npm install -g typescript
 npm install
 ```
-
+## Running the project
+```sh
+npm start
+```
--- app.ts
+++ app.ts
@@ -6,16 +6,16 @@ import {Store} from './store';
 
 //THE NON-REACTIVE STORY
 
-let store= new Store<number>(NumberReducer,99);
+let store = new Store<number>(NumberReducer, 99);
 
 console.log(store.getState());
 
-let unsubscribe = store.subscribe(()=>{
-    console.log('non-reactive subscription : ' + store.getState());
+let unsubscribe = store.subscribe(() => {
+  console.log('non-reactive subscription : ' + store.getState());
 })
 
-store.dispatch({type:'INCREMENT'});
-store.dispatch({type:'DECREMENT'});
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
 store.dispatch({ type: 'PLUS', payload: 7 });
 
 unsubscribe();
@@ -28,17 +28,17 @@ console.log(store.getState());
 
 
 //THE REACTIVE STORY
-let rxStore= new RxStore<number>(NumberReducer,99);
+let rxStore = new RxStore<number>(NumberReducer, 99);
 
 console.log(rxStore.getState());
 
 
-rxStore.subscribe(()=>{
-    console.log('reactive subscription : ' + rxStore.getState());
+rxStore.subscribe(() => {
+  console.log('reactive subscription : ' + rxStore.getState());
 })
 
-rxStore.dispatch({type:'INCREMENT'});
-rxStore.dispatch({type:'DECREMENT'});
+rxStore.dispatch({ type: 'INCREMENT' });
+rxStore.dispatch({ type: 'DECREMENT' });
 rxStore.dispatch({ type: 'PLUS', payload: 7 });
 console.log(rxStore.getState());
 
